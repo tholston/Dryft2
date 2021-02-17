390a6573c7ca8dfb0615a9abd87a89df3df3516c
Errol Sayre <errol.sayre@gmail.com>
2021-02-17 00:04:03 -0600

    Create Constants.php

---
+++ Constants.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * constants.php
+ *
+ * List of constants for use in the DRyft system.
+ */
+
+namespace DRyft;
+
+// Environments
+const DEVELOPMENT = 'Dev';
+const PRODUCTION  = 'Prod';
+
+// DB elements
+const DB_DEV_USER     = 'dryft';
+const DB_DEV_PASSWORD = 'ADeveloperPassword';
+const DB_DEV_HOST     = 'db';
+const DB_DEV_SCHEMA   = 'db';
+
+// User types
+const USER_TYPE_CLIENT = 'Client';
+const USER_TYPE_COORDINATOR = 'Coordinator';
+const USER_TYPE_DRIVER = 'Driver';
