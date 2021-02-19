7f71f5738941d10cafc24cc537878f60bb942cbc
Errol Sayre <errol.sayre@gmail.com>
2021-02-18 22:33:37 -0600

    Display the session user on the front-page

--- public/index.php
+++ public/index.php
@@ -6,9 +6,19 @@
  * Initial page for users. Should redirect based on session status and user type.
  */
 
-require_once( '../bootstrap.php' );
-?>
-<h1>Nothing to see here</h1>
-<?php
+require_once('../bootstrap.php');
+
+// determine if we have a user
+$user = DRyft\Session::getSession()->getUser();
+if (!$user) {
+	echo '<h1>Nothing to see here</h1>';
+} else {
+	echo '<h1>Welcome ' . $user->username() . '</h1>';
+	echo '<pre>';
+	print_r($user);
+	echo '</pre>';
+}
 
 include '../testing_links.html';
+
+echo DRyft\Linker::urlPath();
