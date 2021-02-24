de86a674561be091826601e79c4a88b976548818
Errol Sayre <errol.sayre@gmail.com>
2021-02-23 20:56:39 -0600

    Import production elements from team

--- constants.php
+++ constants.php
@@ -17,6 +17,12 @@ const DB_DEV_PASSWORD = 'ADeveloperPassword';
 const DB_DEV_HOST     = 'db';
 const DB_DEV_SCHEMA   = 'dryft';
 
+// Production DB elements
+const DB_PROD_USER     = 'gamestonk562';
+const DB_PROD_PASSWORD = 'olemiss2021';
+const DB_PROD_HOST     = 'localhost';
+const DB_PROD_SCHEMA   = 'gamestonk562';
+
 // User types
 const USER_TYPE_CLIENT      = 'Client';
 const USER_TYPE_COORDINATOR = 'Coordinator';
