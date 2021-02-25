35b7ee7c95123b769f625c89ad96172eba38e91c
Omegabacklash <nsouth1324@gmail.com>
2021-02-24 22:38:07 -0600

    externalaccess function skeleton created for other files to interact with address information

--- public/address.php
+++ public/address.php
@@ -7,25 +7,37 @@
  *
  * @author Noah South
  */
-    include_once "../DRyft/Database/Connection.php";
-    $db = getConnection();
+    $db = \Database\Connection::getConnection();
     $edit_state = false;
 
     if (isset($_GET['edit'])){
         $id = $_GET['edit'];
-
-        $rec = mysqli_query($db, "SELECT * FROM locations WHERE id=$id");
-        $record = mysqli_fetch_array($rec);
-        $id = $record['LOCATION_ID'];
-        $Lati = $record['latitude'];
-        $Longi = $record['longitude'];
-        $Nickn = $record['nickname'];
-        $Linone = $record['line1'];
-        $Lintwo = $record['line2'];
-        $Cit = $record['city'];
-        $Stat = $record['state'];
-        $Zipc = $record['zip'];
-
+        if($id != NULL){
+            $edit_state = true;
+            $rec = mysqli_query($db, "SELECT * FROM locations WHERE id=$id");
+            $record = mysqli_fetch_array($rec);
+            $id = $record['LOCATION_ID'];
+            $Lati = $record['latitude'];
+            $Longi = $record['longitude'];
+            $Nickn = $record['nickname'];
+            $Linone = $record['line1'];
+            $Lintwo = $record['line2'];
+            $Cit = $record['city'];
+            $Stat = $record['state'];
+            $Zipc = $record['zip'];
+        }
+        else{
+            $edit_state = false;
+            $id = NULL;
+            $Lati = NULL;
+            $Longi = NULL;
+            $Nickn = NULL;
+            $Linone = NULL;
+            $Lintwo = NULL;
+            $Cit = NULL;
+            $Stat = NULL;
+            $Zipc = NULL;
+        }
     }
 ?>
 
@@ -72,25 +84,26 @@
     <form method="post" action>
         <input type="hidden" id="id" name="id" value="<?php echo $id; ?>">
         <label for="Lati">Latitude: </label>
-        <input type="text" id="Lati" name="Lati" value="<?php echo $Lati; ?>"><br>
+        <input type="text" id="Lati" name="Lati" value="<?php echo $Lati; ?>" required><br>
         <label for="Longi">Longitude: </label>
-        <input type="text" id="Longi" name="Longi" value="<?php echo $Longi; ?>"><br>
+        <input type="text" id="Longi" name="Longi" value="<?php echo $Longi; ?>" required><br>
         <label for="Nickn">Nickname: </label>
-        <input type="text" id="Nickn" name="Nickn" value="<?php echo $Nickn; ?>"><br>
+        <input type="text" id="Nickn" name="Nickn" value="<?php echo $Nickn; ?>" required><br>
         <label for="Linone">Line 1: </label>
-        <input type="text" id="Linone" name="Linone" value="<?php echo $Linone; ?>"><br>
+        <input type="text" id="Linone" name="Linone" value="<?php echo $Linone; ?>" required><br>
         <label for="Lintwo">Line 2: </label>
-        <input type="text" id="Lintwo" name="Lintwo" value="<?php echo $Lintwo; ?>"><br>
+        <input type="text" id="Lintwo" name="Lintwo" value="<?php echo $Lintwo; ?>" required><br>
         <label for="Cit">City: </label>
-        <input type="text" id="Cit" name="Cit" value="<?php echo $Cit; ?>"><br>
+        <input type="text" id="Cit" name="Cit" value="<?php echo $Cit; ?>" required><br>
         <label for="Stat">State: </label>
-        <input type="text" id="Stat" name="Stat" value="<?php echo $Stat; ?>"><br>
+        <input type="text" id="Stat" name="Stat" value="<?php echo $Stat; ?>" required><br>
         <label for="Zipc">Zipcode: </label>
-        <input type="text" id="Zipc" name="Zipc" value="<?php echo $Zipc; ?>"><br>
+        <input type="text" id="Zipc" name="Zipc" value="<?php echo $Zipc; ?>" required><br>
         <?php if ($edit_state == false): ?>
-            <input type="submit" name="addLoc" class="btn">
+            <button type="submit" name="addLoc" class="btn">Add</button>
         <?php else: ?>
-            <input type="submit" name="alter" class="btn">
+            <button type="submit" name="alter" class="btn">Update</button>
+            <button name="unselect" class="btn"><a href="address.php?edit=<?php echo NULL ?>">Unselect</a></button>
         <?php endif ?>
     </form>
 </div>
@@ -123,5 +136,18 @@
 
         $query = "UPDATE locations SET latitude='$PLati', longitude='$PLongi', nickname='$PNick', line1='$PLinone', line2='$PLintwo', city='$PCit', state='$PStat', zip='$PZipc' WHERE LOCATION_ID='$PID'";
         mysqli_query($db, $query);
+    }
+
+    //function for external access by other sections of code.
+    function externalaccess($id, $decedit){
+        if($decedit == 0){
+            //if the person simply wishes to access the information
+        }
+        if($decedit == 1){
+            //if the person accesses information with intent to edit
+        }
+        if($decedit != 0 && $decedit != 1){
+            //navigate back to location
+        }
     }
 ?>
