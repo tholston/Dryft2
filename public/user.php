de2c26bec388f24694225bc266b2719feab46661
Errol Sayre <errol.sayre@gmail.com>
2021-02-27 18:07:43 -0600

    Migrate to HTML snippets for view content

--- public/user.php
+++ public/user.php
@@ -56,38 +56,16 @@ if (!$user || !$user->isCoordinator()) {
 	}
 
 	if ($action == ACTION_EDIT) {
-		// display the edit form for the selected user
-?>
-		<h1>Edit <?= $selectedUser->firstName ?> <?= $selectedUser->lastName ?> (<?= $selectedUser->id() ?>)</h1>
-		<form method="POST" action="user.php?id=<?= $selectedUser->id() ?>&action=update" class="needs-validation" novalidate>
-			<div class="row g-3">
-				<div class="col-sm-6">
-					<label for="userType" class="form-label">User Type</label>
-					<select class="form-select" id="userType" required="true">
-						<option value="">Choose...</option>
-						<option <?= $selectedUser->isCoordinator() ? "selected" : "" ?>>Coordinator</option>
-						<option <?= $selectedUser->isDriver() ? " selected" : "" ?>>Driver</option>
-						<option <?= $selectedUser->isClient() ? " selected" : "" ?>>Client</option>
-					</select>
-					<div class="invalid-feedback">Please select a valid country.</div>
-				</div>
-				<div class="col-sm-6">
-					<label for="firstName" class="form-label">First name</label>
-					<input type="text" class="form-control" id="firstName" placeholder="" value="" required="">
-					<div class="invalid-feedback">Valid first name is required.</div>
-				</div>
-				<div class="col-sm-6">
-					<label for="lastName" class="form-label">Last name</label>
-					<input type="text" class="form-control" id="lastName" placeholder="" value="" required="">
-					<div class="invalid-feedback">Valid last name is required.</div>
-				</div>
-			</div>
-			<hr class="my-4">
-			<button class="w-100 btn btn-primary btn-lg" type="submit">Update user</button>
-		</form>
-		<script src="js/form-validation.js"></script>
 
-	<?php
+		// display the edit form for the selected user
+		// setup the submit action
+		$selectedAction = ACTION_UPDATE;
+		// setup the header label
+		$headerLabel = 'Edit ' . $selectedUser->firstName . ' ' . $selectedUser->lastName . ' (' . $selectedUser->id() . ')';
+		// setup the submit label
+		$submitLabel = 'Update user';
+		// include the form snippet
+		include '../views/user-edit.html';
 	} elseif ($action == ACTION_UPDATE) {
 		// load updates for the user from the request
 		echo '<h1>WILL IT UPDATE?</h1>';
@@ -95,38 +73,24 @@ if (!$user || !$user->isCoordinator()) {
 		// load data to create a new user
 		echo '<h1>WILL IT CREATE?</h1>';
 	} elseif ($action == ACTION_NEW) {
+
 		// display the edit form for an empty user
-		echo '<h1>Create a new user</h1>';
+		$selectedUser = new User();
+
+		// setup the submit action
+		$selectedAction = ACTION_CREATE;
+		// setup the header label
+		$headerLabel = 'Create new user';
+		// setup the submit label
+		$submitLabel = 'Create user';
+		// include the form snippet
+		include '../views/user-edit.html';
 	} else {
 		// Present a list of the users in the system
 		$users = User::getUsers();
-	?>
-		<h1>Existing users</h1>
-		<table class="table table-striped">
-			<thead>
-				<tr>
-					<th>ID</th>
-					<th>First Name</th>
-					<th>Last Name</th>
-					<th>Username</th>
-					<th>Actions</th>
-				</tr>
-			</thead>
-			<tbody>
-				<?php foreach ($users as $item) { ?>
-					<tr>
-						<td><?= $item->id() ?></td>
-						<td><?= $item->firstName ?></td>
-						<td><?= $item->lastName ?></td>
-						<td><?= $item->username() ?></td>
-						<td>
-							<form method="POST" action="user.php?id=<?= $item->id() ?>&action=edit"><button type="submit" class="btn btn-sm btn-primary">Edit</button></form>
-						</td>
-					</tr>
-				<?php } ?>
-			</tbody>
-		</table>
-<?php
+
+		// include the user list snippet
+		include '../views/user-list.html';
 	}
 }
 
