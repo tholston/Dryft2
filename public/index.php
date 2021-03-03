206b38b28a7ae9a12ca342d541d752b0479bd8f4
Errol Sayre <esayre@olemiss.edu>
2021-03-02 18:09:06 -0600

    Build main links for coordinator

--- public/index.php
+++ public/index.php
@@ -31,7 +31,21 @@ if (!$user) {
 	echo '<h1>Welcome to DRyft</h1>';
 	echo '<p><a href="' . $linker->urlPath() . 'login.php">Please login to begin.</a></p>';
 } else {
+
 	echo '<h1>Welcome ' . $user->username() . '</h1>';
+
+	// Put some shortcuts that make sense for the given user
+	if ($user->isCoordinator()) {
+?>
+		<p>Welcome <?= $user->firstName ?>. As a coordinator you can manage all aspects of the DRyft application. Please make use of the links below or the navigation above.</p>
+		<ul>
+			<li><a href="payments.php">Manage driver payments</a></li>
+			<li><a href="user.php">List all users</a></li>
+			<li><a href="driver.php">List all drivers</a></li>
+			<li><a href="address.php">Manage addresses</a></li>
+		</ul>
+<?php
+	}
 }
 
 // add page footer
